c15a729845c214ce82354c1b981183102efc05db
rga-king <staxhappy@gmail.com>
2023-03-06 22:25:25 +0000

    artwork page updates

---
+++ js/artwork-meta.js
@@ -0,0 +1,121 @@
+const artGallery = {
+  lateAutumnGrapevine: {
+    id: "001",
+    heading: "LATE AUTUMN GRAPEVINE",
+    description: "Vitis Vinifera. The leaves are turning and the grapes are over; a few dried, empty skins remain, the fruit having been eaten by birds. This branch is one season's growth and casts a precise, dark shadow in the strong Spanish sun.",
+    medium: "Colour pencil and graphite",
+    size: "Approx 53cmW X 38cmH",
+    original: "SOLD",
+    print: "£80",
+    images: {
+      preview: "late-autumn-grapevice.jpeg",
+      main: "late-autumn-grapevine-wm.jpeg"
+    }
+  },
+  palmShadows: {
+    id: "002",
+    heading: "PALM SHADOWS",
+    description: "Arecaceae. Washingtonia Robusta. This palm produces large fan shaped, pleated leaves and the shadows cast by the upper leaves form a zig zag shifting pattern.",
+    medium: "Colour pencil",
+    size: "Approx 35cmW X 54cmH",
+    original: "Not available for sale",
+    print: "£75",
+    images: {
+      preview: "palm-shadows.jpeg",
+      main: "palm-shadows-wm.jpeg"
+    }
+  },
+  wigandiaCaracasana: {
+    id: "003",
+    heading: "WIGANDIA CARACASANA",
+    description: "The play of strong sunlight on and through the large leaves of this evergreen shrub was the inspiration for this picture. This plant was found in an Andalusian garden where it grows to 3 metres and bears purple flowers in the summer.",
+    medium: "Colour pencil",
+    size: "Approx 34cmW X 55cmH",
+    original: "Not available for sale",
+    print: "£80",
+    images: {
+      preview: "wigandia-caracasana.jpeg",
+      main: "wigandia-caracasana-wm.jpeg"
+    }
+  },
+  bindweedOnCotoneaster: {
+    id: "004",
+    heading: "BINDWEED ON COTONEASTER",
+    description: "Calystegia sepium. An attractive menace that grows up anything in a wild part of my garden in Suffolk.",
+    medium: "Colour pencil and graphite",
+    size: "Approx 27cmW X 40cmH",
+    original: "SOLD",
+    print: "£45",
+    images: {
+      preview: "bindweed-on-cotoneaster.jpeg",
+      main: "bindweed-on-cotoneaster-wm.jpeg"
+    }
+  },
+  weepingWillow: {
+    id: "005",
+    heading: "WEEPING WILLOW",
+    description: "Salix Babylonica. This picture shows the different seasons of a large specimen that grows in my Suffolk garden.",
+    medium: "Colour pencil",
+    size: "Approx 51cmW X 74cmH",
+    original: "Not available for sale",
+    print: "&#163;60.00",
+    images: {
+      preview: "weeping-willow.jpeg",
+      main: "weeping-willow-wm.jpeg"
+    }
+  },
+  spanishGrapevine: {
+    id: "006",
+    heading: "SPANISH GRAPEVINE",
+    description: "Vitis Vinifera. This vine was growing on a trellis in an Andalusian garden with an abundance of ripening grapes hanging down, untouched and with their bloom intact.",
+    medium: "Colour pencil",
+    size: "Approx 39cmW X 61cmH",
+    original: "Not available for sale",
+    print: "£75",
+    images: {
+      preview: "spanish-grapevine.jpeg",
+      main: "spanish-grapevine-wm.jpeg"
+    }
+  },
+  monsteraDeliciosaLeaf: {
+    id: "007",
+    heading: "MONSTERA DELICIOSA LEAF",
+    description: "This giant 'cheese plant' was growing outdoors in Andalusia. The leaf is life size.",
+    medium: "Colour pencil",
+    size: "Approx 61cmW X 81cmH",
+    original: "£1,750",
+    print: "£100",
+    images: {
+      preview: "monstera-deliciosa-leaf.jpeg",
+      main: "monstera-deliciosa-leaf-wm.jpeg"
+    }
+  },
+  monsteraDeliciosaSpadixAndFruit: {
+    id: "008",
+    heading: "MONSTERA DELICIOSA SPADIX AND FRUIT",
+    description: "This climbing 'cheese plant' was growing up a carob tree in Andalusia supported by its aerial roots.  As the fruit ripened it split, revealing a giant's causeway structure and emitting a sweet scent.  It would never grow outdoors in England ",
+    medium: "Colour pencil",
+    size: "Approx 41cmW X 71cmH",
+    original: "£800",
+    print: "£60",
+    images: {
+      preview: "monstera-deliciosa-spadix-and-fruit.jpeg",
+      main: "monstera-deliciosa-spadix-and-fruit-wm.jpeg"
+    }
+  }
+}
+
+
+    // <name>: {
+  //   id: "00",
+  //   heading: "",
+  //   description: "",
+  //   medium: "Colour pencil",
+  //   size: "Approx ",
+  //   original: "",
+  //   print: "£",
+  //   images: {
+  //     preview: ".jpeg",
+  //     main: "-wm.jpeg"
+  //   }
+  // }
