809ae26aad18ee7382c315c3a058a79cceaadab8
rga-king <rga.king@gmail.com>
2024-03-09 15:32:11 +0000

    add new fabric entry

--- js/fabric-metadata.js
+++ js/fabric-metadata.js
@@ -1,46 +1,54 @@
 const fabricGallery = {
-  fabric_sage: {
-    heading: "SAGE",
-    description: "Coordinates with <a href='./cushions.html#sage3-cushion' class='link'>Sage 3 cushion</a> and <a href='./cushions.html#sageBolster-cushion' class='link'>Sage Bolster cushion</a>",
-    material: "60% cotton 40% linen",
-    width: "142 cms",
-    individualMotif: "2.5 X 10 cms",
-    repeat: "19 cms",
-    perMetre: "£58", 
-    img: "sage-fabric.jpg"
+  fabric_sage3: {
+    heading: 'SAGE',
+    description:
+      "Coordinates with <a href='./cushions.html#sage3-cushion' class='link'>Sage 3 cushion</a> and <a href='./cushions.html#sageBolster-cushion' class='link'>Sage Bolster cushion</a>",
+    material: '60% cotton 40% linen',
+    width: '142 cms',
+    individualMotif: '2.5 X 10 cms',
+    repeat: '19 cms',
+    perMetre: '£58',
+    img: 'sage-fabric.jpg'
   },
   fabric_thalictrum: {
-    heading: "THALICTRUM",
-    description: "Coordinates with <a href='./cushions.html#thalictrum-cushion' class='link'>Thalictrum cushion</a>",
-    material: "60% cotton 40% linen",
-    width: "142 cms",
-    individualMotif: "8.5 X 13 cms",
-    repeat: "22 cms",
-    perMetre: "£58", 
-    img: "thalictrum-fabric.jpg"
+    heading: 'THALICTRUM',
+    description:
+      "Coordinates with <a href='./cushions.html#thalictrum-cushion' class='link'>Thalictrum cushion</a>",
+    material: '60% cotton 40% linen',
+    width: '142 cms',
+    individualMotif: '8.5 X 13 cms',
+    repeat: '22 cms',
+    perMetre: '£58',
+    img: 'thalictrum-fabric.jpg'
   },
   fabric_allium: {
-    heading: "ALLIUM BESPOKE FABRIC",
-    description: "A three metre drop for a two metre wide window.",
-    guidePrice: "£1,500",
-    img: "allium-fabric-bespoke.jpg"
+    heading: 'ALLIUM BESPOKE FABRIC',
+    description: 'A three metre drop for a two metre wide window.',
+    guidePrice: '£1,500',
+    img: 'allium-fabric-bespoke.jpg'
   },
   fabric_sorbaria: {
-    heading: "SORBARIA BESPOKE FABRIC",
-    description: "A two metre drop for a two metre wide window.",
-    guidePrice: "£1,200",
-    img: "sorbaria-fabric-bespoke.jpg"
+    heading: 'SORBARIA BESPOKE FABRIC',
+    description: 'A two metre drop for a two metre wide window.',
+    guidePrice: '£1,200',
+    img: 'sorbaria-fabric-bespoke.jpg'
   },
   fabric_cotinus: {
-    heading: "COTINUS BESPOKE FABRIC",
-    description: "A two metre drop for a one and half metre wide window.",
-    guidePrice: "£1,000",
-    img: "cotinus-fabric-bespoke.jpg"
+    heading: 'COTINUS BESPOKE FABRIC',
+    description: 'A two metre drop for a one and half metre wide window.',
+    guidePrice: '£1,000',
+    img: 'cotinus-fabric-bespoke.jpg'
   },
   fabric_honeysuckle: {
-    heading: "HONEYSUCKLE BESPOKE FABRIC",
-    description: "A one and a half metre drop for metre wide window.",
-    guidePrice: "£700",
-    img: "honeysuckle-fabric-bespoke.jpg"
+    heading: 'HONEYSUCKLE BESPOKE FABRIC',
+    description: 'A one and a half metre drop for metre wide window.',
+    guidePrice: '£700',
+    img: 'honeysuckle-fabric-bespoke.jpg'
+  },
+  fabric_sage3_gladiolus: {
+    heading: 'Sage/Gladiolus',
+    description: 'A double sided two metre drop door curtain.',
+    guidePrice: '£800',
+    img: 'sage3-gladiolus-fabric-bespoke.jpg'
   }
-}
+};
